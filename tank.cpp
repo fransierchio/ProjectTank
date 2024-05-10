2d6ef7bc8a915ad1a4d2f4c50cea6a58f1d1b621
fransierchio <sierchiofrancesco@gmail.com>
2024-05-10 00:05:30 -0400

    Sin bugs y optimizado

--- tank.cpp
+++ tank.cpp
@@ -9,6 +9,7 @@ void StopButtom(int x, int y);
 bool isInsideCircle(int x, int y, int centerX, int centerY, int radius);
 void fillTank();
 
+//variables globales
 bool fillingInProgress = false;
 bool stopFilling = true;
 int tankHeight = 610; 
@@ -17,7 +18,6 @@ int waterLevel = tankTop + tankHeight;
 
 
 int main() {
-    //PRIMERA INTERFAZ
     // Tamaño de la pantalla
     int width = 1300;
     int height = 900;
@@ -25,19 +25,14 @@ int main() {
 
     //IMAGEN DEL TANQUE DE AGUA
     drawBackground();
+
+    int x, y;
     while (!kbhit()) 
     {
-        int x, y;
-        if (ismouseclick(WM_LBUTTONDOWN)) 
-            {
-                getmouseclick(WM_LBUTTONDOWN, x, y);
-                FillButtom(x, y);
-                StopButtom(x, y);
-            }
+        getmouseclick(WM_LBUTTONDOWN, x, y);
+        FillButtom(x, y);
+        StopButtom(x, y);
     }
-
-    // Esperar a que el usuario presione una tecla antes de cerrar la ventana
-    getch();
 
     return 0;
 }
@@ -46,52 +41,7 @@ void drawBackground()
 {
     int width = getmaxx();
     int height = getmaxy();
-
-    //Size of 
-    int bgWidth = width;  
-    int bgHeight = height;
-
-    readimagefile("background.jpg", 0, 0, bgWidth,bgHeight);
-}
-
-void FillButtom(int x, int y) 
-{
-    
-    int fillButtonCenterX = 760; 
-    int fillButtonCenterY = 326; 
-    int fillButtonRadius = 56;
-    setcolor(RED);
-    circle(fillButtonCenterX, fillButtonCenterY, fillButtonRadius);
-
-        // Verificar si el clic del mouse está dentro del botón circular
-        if (isInsideCircle(x, y, fillButtonCenterX, fillButtonCenterY, fillButtonRadius)) 
-            {
-                if (!fillingInProgress) 
-                { 
-                    fillingInProgress = true; 
-                    thread fillThread(fillTank); 
-                    fillThread.detach();
-                    stopFilling = false;
-                }
-            }
-            
-}
-
-void StopButtom(int x, int y) 
-{
-    
-    int StopButtonCenterX = 964; 
-    int StopButtonCenterY = 320; 
-    int StopButtonRadius = 56;
-    setcolor(RED);
-    circle(StopButtonCenterX, StopButtonCenterY, StopButtonRadius);
-
-        // Verificar si el clic del mouse está dentro del botón circular
-        if (isInsideCircle(x, y, StopButtonCenterX, StopButtonCenterY, StopButtonRadius)) 
-            {
-                cout << "Botón de detención presionado" << endl;
-                stopFilling = true;
-            }
+    readimagefile("background.jpg", 0, 0, width,height);
 }
 
 bool isInsideCircle(int x, int y, int centerX, int centerY, int radius) {
@@ -100,17 +50,42 @@ bool isInsideCircle(int x, int y, int centerX, int centerY, int radius) {
     return dx * dx + dy * dy <= radius * radius;
 }
 
+void FillButtom(int x, int y) 
+{
+    int fillButtonCenterX = 760; int fillButtonCenterY = 326; int fillButtonRadius = 56;
+    // Verificar si el clic del mouse está dentro del botón circular
+    if (isInsideCircle(x, y, fillButtonCenterX, fillButtonCenterY, fillButtonRadius)) 
+        {
+            if (!fillingInProgress) 
+            { 
+                fillingInProgress = true; 
+                thread fillThread(fillTank); 
+                fillThread.detach();
+                stopFilling = false;
+            }
+        }   
+}
+
+void StopButtom(int x, int y) 
+{
+    int StopButtonCenterX = 964; int StopButtonCenterY = 320; int StopButtonRadius = 56;
+        // Verificar si el clic del mouse está dentro del botón circular
+        if (isInsideCircle(x, y, StopButtonCenterX, StopButtonCenterY, StopButtonRadius)) 
+            {
+                stopFilling = true;
+            }
+}
+
 void fillTank() {
     int tankWidth = 230; 
     int tankLeft =186;
     int fillSpeed = 4; // Velocidad de llenado
-    int maxWaterLevel = tankTop; // Nivel máximo de agua en el tanque
 
     setcolor(LIGHTCYAN);
     setfillstyle(SOLID_FILL, LIGHTCYAN);
     
     // Llenar el tanque mientras stopFilling sea falso y el nivel de agua sea mayor que el nivel máximo
-    while (!stopFilling && waterLevel > maxWaterLevel) { 
+    while (!stopFilling && waterLevel > tankTop) { 
         bar(tankLeft, waterLevel - fillSpeed, tankLeft + tankWidth, waterLevel);
         waterLevel -= fillSpeed; // Disminuir el nivel de agua
         delay(50);
